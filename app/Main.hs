6dcf6d0df28c4a3592b33376b241a985b07c5e6d
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-08-30 13:40:08 -0700

    Intial commit

---
+++ app/Main.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Control.Lens
+import           Control.Monad
+import           Data.Aeson                 as A
+import           Data.Aeson.Lens
+import           Development.Shake
+import           Development.Shake.Classes
+import           Development.Shake.Forward
+import           Development.Shake.FilePath
+import           GHC.Generics               (Generic)
+import           Slick
+import qualified Data.Text                  as T
+
+outputFolder :: FilePath
+outputFolder = "docs/"
+
+-- | Data for the index page
+data IndexInfo =
+  IndexInfo
+    { posts :: [Post]
+    } deriving (Generic, Show, FromJSON, ToJSON)
+
+-- | Data for a blog post
+data Post =
+    Post { title   :: String
+         , author  :: String
+         , content :: String
+         , url     :: String
+         , date    :: String
+         , image   :: Maybe String
+         }
+    deriving (Generic, Eq, Ord, Show, FromJSON, ToJSON, Binary)
+
+-- | given a list of posts this will build a table of contents
+buildIndex :: [Post] -> Action ()
+buildIndex posts' = do
+  indexT <- compileTemplate' "site/templates/index.html"
+  let indexInfo = IndexInfo {posts = posts'}
+      indexHTML = T.unpack $ substitute indexT (toJSON indexInfo)
+  writeFile' (outputFolder </> "index.html") indexHTML
+
+-- | Find and build all posts
+buildPosts :: Action [Post]
+buildPosts = do
+  pPaths <- getDirectoryFiles "." ["site/posts//*.md"]
+  forP pPaths buildPost
+
+-- | Load a post, process metadata, write it to output, then return the post object
+-- Detects changes to either post content or template
+buildPost :: FilePath -> Action Post
+buildPost srcPath = cacheAction ("build" :: T.Text, srcPath) $ do
+  liftIO . putStrLn $ "Rebuilding post: " <> srcPath
+  postContent <- readFile' srcPath
+  -- load post content and metadata as JSON blob
+  postData <- markdownToHTML . T.pack $ postContent
+  let postUrl = T.pack . dropDirectory1 $ srcPath -<.> "html"
+      withPostUrl = _Object . at "url" ?~ String postUrl
+  -- Add additional metadata we've been able to compute
+  let fullPostData = withPostUrl $ postData
+  template <- compileTemplate' "site/templates/post.html"
+  writeFile' (outputFolder </> T.unpack postUrl) . T.unpack $ substitute template fullPostData
+  -- Convert the metadata into a Post object
+  convert fullPostData
+
+-- | Copy all static files from the listed folders to their destination
+copyStaticFiles :: Action ()
+copyStaticFiles = do
+    filepaths <- getDirectoryFiles "./site/" ["images//*", "css//*", "js//*"]
+    void $ forP filepaths $ \filepath ->
+        copyFileChanged ("site" </> filepath) (outputFolder </> filepath)
+
+-- | Specific build rules for the Shake system
+--   defines workflow to build the website
+buildRules :: Action ()
+buildRules = do
+  allPosts <- buildPosts
+  buildIndex allPosts
+  copyStaticFiles
+
+-- | Kick it all off
+main :: IO ()
+main = do
+  let shOpts = forwardOptions $ shakeOptions { shakeVerbosity = Chatty}
+  shakeArgsForward shOpts buildRules
