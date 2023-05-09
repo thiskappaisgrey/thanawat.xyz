991fedd92db3487a63739558908667819511ea96
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2023-05-09 14:49:54 -0700

    Add an atom feed for posts

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,15 @@
 module Main where
 
 -- import Control.Lens
+
+-- in case I want to remove directory recursively on rebuild
+
+import Atom (renderAtomFeed)
 import Control.Monad
+import Control.Monad.Logger (runStdoutLoggingT)
 import Data.Aeson as A
 import Data.Aeson.Lens
+import Data.Default (def)
 import qualified Data.Map as M
 import Data.Org as O
 import qualified Data.Org.Lucid as LO
@@ -18,24 +24,22 @@ import Development.Shake.Classes
 import Development.Shake.FilePath
 import Development.Shake.Forward
 import qualified Lucid as L
+import Network.Wai.Application.Static
+import Network.Wai.Handler.Warp
+import Optics.Core ((%), (&), (.~), (^.))
 import qualified Page as P
 import qualified Page.About as PA
 import qualified Page.Index as PI
+import System.Directory (removeDirectoryRecursive)
 import Types
-import Network.Wai.Handler.Warp
-import Network.Wai.Application.Static
-import System.Directory (removeDirectoryRecursive) -- in case I want to remove directory recursively on rebuild
-
 import qualified Web.Tailwind as Tailwind
-import Control.Monad.Logger (runStdoutLoggingT)
-  
-import Optics.Core ((%), (.~), (^.), (&))
-import Data.Default (def)
+import Data.Time.Format.ISO8601 (iso8601Show)
+import Data.Time (getCurrentTime)
 
 outputFolder :: FilePath
 outputFolder = "build/"
 
---Utility Functions------------------------------------------------------------
+-- Utility Functions------------------------------------------------------------
 -- TODO I need to extract the metadata for the index
 maybeToEither :: a -> Maybe b -> Either a b
 maybeToEither msg Nothing = Left msg
@@ -44,23 +48,26 @@ maybeToEither msg (Just val) = Right val
 maybeToValidation :: String -> Maybe a -> Validation OrgMetaError a
 maybeToValidation msg Nothing = Failure (OrgMetaError msg)
 maybeToValidation msg (Just val) = V.Success val
+
 -- Basically allow you to accumulate the error in a string like:
 -- title, date ...
 newtype OrgMetaError = OrgMetaError String
 
 instance Semigroup OrgMetaError where
-  (OrgMetaError a) <>  (OrgMetaError b)= OrgMetaError $ mconcat [a, ", ", b]
+  (OrgMetaError a) <> (OrgMetaError b) = OrgMetaError $ mconcat [a, ", ", b]
 
 instance Monoid OrgMetaError where
-  mappend   = (<>)
+  mappend = (<>)
   mempty = OrgMetaError ""
 
 getMetaData :: M.Map T.Text T.Text -> T.Text -> Validation OrgMetaError Post
-getMetaData m postUrl = Post <$>
-  maybeToValidation "title" (M.lookup "TITLE" m)
-  <*> pure postUrl
-  <*> maybeToValidation "date" (M.lookup "DATE" m)
+getMetaData m postUrl =
+  Post
+    <$> maybeToValidation "title" (M.lookup "TITLE" m)
+    <*> pure postUrl
+    <*> maybeToValidation "date" (M.lookup "DATE" m)
 
+-- TODO maybe consider writing Text versions of writeFile'?
 convertOrg :: T.Text -> T.Text -> Action Post
 convertOrg postUrl orgText = do
   let orgFile = O.org orgText
@@ -74,15 +81,16 @@ convertOrg postUrl orgText = do
         Failure (OrgMetaError str) -> fail $ mconcat ["Post ", T.unpack postUrl, " is missing: ", str]
         V.Success m -> do
           let post = P.post $ LO.body LO.defaultStyle file
-          writeFile' (outputFolder </> T.unpack postUrl)  post
+          writeFile' (outputFolder </> T.unpack postUrl) post
           return m
+
 -- Build Rules -----------------------------------------------------------
+
 -- | Find and build all posts
 buildPosts :: Action [Post]
 buildPosts = do
   pPaths <- getDirectoryFiles "." ["site/posts//*.org"]
   forP pPaths buildPost
-
 
 -- | Load a post, process metadata, write it to output, then return the post object
 -- Detects changes to either post content or template
@@ -94,7 +102,9 @@ buildPost srcPath = cacheAction ("build" :: T.Text, srcPath) $ do
   let postUrl = T.pack . dropDirectory1 $ srcPath -<.> "html"
   -- withPostUrl = _Object . at "url" ?~ String postUrl
   -- helper to take the content and output an org file
-  convertOrg postUrl $ T.pack postContent
+  a <- convertOrg postUrl $ T.pack postContent
+  liftIO . putStrLn $ "Post: " <> show a
+  return a
 
 buildIndex :: [Post] -> Action ()
 buildIndex post =
@@ -107,10 +117,10 @@ copyStaticFiles = do
   void $
     forP filepaths $ \filepath ->
       copyFileChanged ("site" </> filepath) (outputFolder </> filepath)
+
 -- Requires a "Partial" type contraint but I don't know which Partial it is..
 readFileText :: FilePath -> Action T.Text
 readFileText x = need [x] >> liftIO (T.IO.readFile x)
-
 
 -- TODO get "templates" as org files, filer out the necessary info then do some stuff with it?
 buildAbout :: Action ()
@@ -129,15 +139,43 @@ buildAbout = do
     Nothing ->
       liftIO $ putStrLn "About File could not be parsed"
 
-
 -- build tailwind
 compileTailwindCss :: FilePath -> [FilePath] -> Action ()
-compileTailwindCss cssPath genPaths = liftIO $ runStdoutLoggingT $
-    do Tailwind.runTailwind $ def
-                  & Tailwind.tailwindConfig % Tailwind.tailwindConfigContent .~ genPaths
-                  & Tailwind.tailwindOutput .~ cssPath
-                  & Tailwind.tailwindMode .~ Tailwind.Production
+compileTailwindCss cssPath genPaths = liftIO $
+  runStdoutLoggingT $
+    do
+      Tailwind.runTailwind $
+        def
+          & Tailwind.tailwindConfig % Tailwind.tailwindConfigContent .~ genPaths
+          & Tailwind.tailwindOutput .~ cssPath
+          & Tailwind.tailwindMode .~ Tailwind.Production
 
+-- TODO atom
+siteAtom :: Atom
+siteAtom =
+  Atom
+    { _title = "thanawat.xyz",
+      _atomUrl = "thanawat.xyz/atom.xml",
+      _updated = "05-09-2023", -- TODO
+      _posts = [],
+      _domain = "https://thanawat.xyz"
+    }
+
+-- build site atom
+buildSiteAtom :: [Post] -> Action ()
+buildSiteAtom posts = do
+  now <- liftIO getCurrentTime
+  writeFile' (outputFolder </> "feed.xml") $
+    TZ.unpack $
+      renderAtomFeed $
+        Atom
+          { _title = "thanawat.xyz",
+            _atomUrl = "/feed.xml",
+            _updated = T.pack $  iso8601Show now,
+            _posts = posts,
+            _domain = "https://thanawat.xyz"
+          } 
+    
 
 -- | Specific build rules for the Shake system
 --   defines workflow to build the website
@@ -146,10 +184,10 @@ buildRules = do
   posts <- buildPosts
   buildAbout
   buildIndex posts
+  buildSiteAtom posts
   copyStaticFiles
   -- TODO the second argument needs to be hall the haskell files
   compileTailwindCss (outputFolder </> "tailwind.css") ["./app/Page/*.hs"]
-
 
 -- | Kick it all off
 main :: IO ()
--- app/Types.hs
+++ app/Types.hs
@@ -1,36 +1,54 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Types where
-import           Data.Aeson                 as A
-import qualified Data.Text                  as T
-import           Data.Org                   (OrgDoc, orgFile, OrgFile (OrgFile))
-import           GHC.Generics               (Generic)
-import           Development.Shake.Classes
-import qualified Data.Map                    as M
+
+import Data.Aeson as A
+import qualified Data.Map as M
+import Data.Org (OrgDoc, OrgFile (OrgFile), orgFile)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Development.Shake.Classes
+import GHC.Generics (Generic)
+
 -- avoid circular dependency
+
 -- | Data for the index page
-newtype IndexInfo =
-  IndexInfo
-    { posts :: [Post]
-    } deriving (Generic, Show, FromJSON, ToJSON)
+newtype IndexInfo = IndexInfo
+  { posts :: [Post]
+  }
+  deriving (Generic, Show, FromJSON, ToJSON)
 
 -- | Data for a blog post
-data Post =
-    Post { title   :: T.Text
-         -- , content :: String
-         , url     ::  T.Text
-         , date    ::   T.Text
-         -- , image   :: Maybe String
-         }
-    deriving (Generic, Eq, Ord, Show, FromJSON, ToJSON, Binary)
+data Post = Post
+  { title :: T.Text,
+    -- TODO the "content" of the post is really the unescaped HTML ouput
+    -- , content :: String
+    -- TODO add summary + content
+    url :: T.Text,
+    date :: T.Text
+    -- , image   :: Maybe String
+  }
+  deriving (Generic, Eq, Ord, Show, FromJSON, ToJSON, Binary)
 
 -- | Data for an About Page
-data AboutSection = AboutSection {
-  sectionHeader :: T.Text
-  , sectionContent :: OrgFile 
-  , sectionImagePath :: Maybe T.Text
-                                 } deriving (Show)
-data About = About {
-  aboutPreamble :: OrgFile
-  , aboutSections :: [AboutSection]
-                   } deriving (Show)
+data AboutSection = AboutSection
+  { sectionHeader :: T.Text,
+    sectionContent :: OrgFile,
+    sectionImagePath :: Maybe T.Text
+  }
+  deriving (Show)
+
+data About = About
+  { aboutPreamble :: OrgFile,
+    aboutSections :: [AboutSection]
+  }
+  deriving (Show)
+
+data Atom = Atom
+  { _title :: Text,
+    _updated :: Text,
+    _atomUrl :: Text,
+    _posts :: [Post],
+    _domain :: Text
+  }
